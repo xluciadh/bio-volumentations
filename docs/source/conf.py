7fc9b9e6284e57b2f4d245d63e743886e616a5bd
Lucia D. Hradecka <lucia.d.hradecka@gmail.com>
2024-04-15 09:51:07 +0200

    publishing at readthedocs (solving sphinx RTD issues)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,9 @@
 # documentation root, use os.path.abspath to make it absolute.
 import os
 import sys
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath(r'..\..'))
-# sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath(r'..\..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
