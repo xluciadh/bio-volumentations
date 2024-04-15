edecd65357f47da099b6e7be5e105c58f2fc46b9
Lucia D. Hradecka <lucia.d.hradecka@gmail.com>
2024-04-15 10:02:14 +0200

    publishing at readthedocs (solving sphinx RTD issues)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath(r'..\..'))
-# sys.path.insert(0, os.path.abspath(r'..\..'))
+sys.path.insert(0, os.path.abspath(r'../../'))
+sys.path.insert(0, os.path.abspath(r'../../bio_volumentations'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
