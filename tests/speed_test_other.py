1dabe16b3b5344a8600e69053f4f4e2aa137d8f8
Lucia D. Hradecka <lucia.d.hradecka@gmail.com>
2024-12-06 12:58:21 +0100

    speed tests (part 2)

--- tests/speed_test_other.py
+++ tests/speed_test_other.py
@@ -33,47 +33,81 @@ import bio_volumentations.augmentations as biovol
 # TorchIO: requires pytorch, torchIO (pip) (had networkx 2.8.8)
 import torchio
 # volumentations: require ???
-# TODO
+import volumentations
 # gunpowder: requires gunpowder (pip) (installed networkx 3.2.1)
-import gunpowder
+# import gunpowder
 
 # torchvision: requires pytorch, torchvision
-import torchvision.transforms.v2 as torchvision_v2
+# import torchvision.transforms.v2 as torchvision_v2
 # albumentations: require albumentations
-import albumentations
+# import albumentations
 
 
-libs = ['biovol', 'torchio', 'volum', 'gunpowder', 'torchvision', 'album']
+# libs = ['biovol', 'torchio', 'volum', 'gunpowder', 'torchvision', 'album']
+libs = ['biovol', 'torchio', 'volum']
+
+# num_repeat = 100
+num_repeat = 10
 
 
 # image_shape_list = [(1, 256, 256, 256), (3, 256, 256, 256), (1, 256, 256, 256, 10), (3, 256, 256, 256, 10)]
 image_shape_list = [(1, 128, 128, 128)]
 
-# num_repeat = 100
-num_repeat = 10
+crop_shape = (100, 100, 100)
+pad_size = 6
+pad_shape = (140, 140, 140)
+rotation_limit = (22.5,) * 3
+translation_limit = (10,) * 3
+scaling_limit = (0.8, 1.2) * 3
+anisotropic_spacing = (1, 0.5, 2)
+blur_sigma_limit = 2
+noise_sigma_limits = (0.001, 1)
+brightness_limit = 0.2
+constrast_limit = 0.2
 
+# https://biovolumentations.readthedocs.io/latest/bio_volumentations.augmentations.html#
 augmentations_biovol = [
-    biovol.RandomCrop(),
-    biovol.Pad(),
+    biovol.RandomCrop(shape=crop_shape, border_mode='constant', p=1),  # crop or pad
+    biovol.Pad(pad_size=pad_size, border_mode='constant', p=1),  # only pad
     biovol.RandomFlip(axes_to_choose=None, p=1),
-    biovol.RandomAffineTransform(angle_limit=[22.5, 22.5, 22.5], translation_limit=[10, 10, 10],
-                          scaling_limit=[.2, .2, .2], spacing=[1, 0.5, 2], p=1),
-    biovol.RandomGaussianBlur(max_sigma=0.8, p=1),
-    biovol.GaussianNoise(var_limit=(0.001, 0.1), mean=0, p=1),
-    biovol.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1),
+    biovol.RandomAffineTransform(angle_limit=rotation_limit, translation_limit=translation_limit,
+                                 scaling_limit=scaling_limit, p=1),  # isotropic data
+    biovol.RandomAffineTransform(angle_limit=rotation_limit, translation_limit=translation_limit,
+                                 scaling_limit=scaling_limit, spacing=anisotropic_spacing, p=1),  # anisotropic data
+    biovol.RandomGaussianBlur(max_sigma=blur_sigma_limit, p=1),
+    biovol.GaussianNoise(var_limit=noise_sigma_limits, mean=0, p=1),
+    biovol.RandomBrightnessContrast(brightness_limit=brightness_limit, contrast_limit=constrast_limit, p=1),
     biovol.Normalize(mean=0, std=1, p=1),
 ]
 
+# https://torchio.readthedocs.io/
 augmentations_torchio = [
-    torchio.Crop(p=1),  # center or random? maybe CroporPad?
-    torchio.Pad(p=1),  # maybe CroporPad?
-    torchio.CropOrPad(p=1),
-    torchio.RandomFlip(axes=[], flip_probability=1.0, p=1),
-    torchio.RandomAffine(scales=[], degrees=[], translation=[], p=1),  # TODO isotropic
-    torchio.RandomBlur(p=1),
-    torchio.RandomNoise(p=1),
+    torchio.CropOrPad(target_shape=crop_shape, padding_mode=0, p=1),  # TODO center or random? no one knows
+    torchio.Pad(padding=pad_size, padding_mode=0, p=1),
+    torchio.RandomFlip(axes=(0, 1, 2), flip_probability=0.5, p=1),
+    torchio.RandomAffine(scales=scaling_limit, degrees=rotation_limit, translation=translation_limit, p=1),
+    # MISSING: anisotropic affine transform (their 'isotropic' has a different meaning)
+    torchio.RandomBlur(std=blur_sigma_limit, p=1),
+    torchio.RandomNoise(mean=0, std=noise_sigma_limits, p=1),
     # MISSING: brightness contrast
     torchio.ZNormalization(p=1)
+]
+
+# https://github.com/ZFTurbo/volumentations/blob/master/volumentations/augmentations/transforms.py
+# TODO: no generated docs + some classes are not documented at all (even in the code)
+rotate_limit_vol = (-22.5, 22.5)
+augmentations_volumentations = [
+    volumentations.RandomCrop(shape=crop_shape, p=1),  # TODO only crop
+    volumentations.PadIfNeeded(shape=pad_shape, border_mode='constant', p=1),  # TODO only pad ??
+    volumentations.Flip(axis=None, p=1),  # this is random variant (no deterministic exists)
+    # MISSING: affine and translation -> using a sequence of rotation and scale at least
+    [volumentations.RandomScale(scaling_limit[:2], p=1),
+     volumentations.Rotate(x_limit=rotate_limit_vol, y_limit=rotate_limit_vol, z_limit=rotate_limit_vol, p=1)],
+    # MISSING: anisotropic affine (or anisotropic anything)
+    volumentations.Blur(blur_limit=(3, 7), p=1),  # TODO: unclear meaning of param blur_limit; is it Gaussian?
+    volumentations.GaussianNoise(var_limit=noise_sigma_limits, mean=0, p=1),
+    volumentations.RandomBrightnessContrast(brightness_limit=brightness_limit, contrast_limit=constrast_limit, p=1),
+    volumentations.Normalize(range_norm=False, p=1),
 ]
 
 
@@ -83,7 +117,7 @@ def get_transformation_list(lib):
     if lib == 'torchio':
         return augmentations_torchio
     if lib == 'volum':
-        pass
+        return augmentations_volumentations
     if lib == 'gunpowder':
         pass
     if lib == 'torchvision':
@@ -98,7 +132,11 @@ def init_compose(lib, transform):
     if lib == 'torchio':
         return torchio.Compose([transform], p=1)
     if lib == 'volum':
-        pass
+        if isinstance(transform, list):
+            # the "affine" transform
+            return volumentations.Compose(transform, p=1.0)
+        else:
+            return volumentations.Compose([transform], p=1.0)
     if lib == 'gunpowder':
         pass
     if lib == 'torchvision':
@@ -117,7 +155,8 @@ def get_input_data(lib, shape):
         # (C, W, H, D)
         return np.random.uniform(low=0, high=1, size=shape.transpose((0, 3, 2, 1)))
     if lib == 'volum':
-        pass
+        # TODO shape
+        return {'image': np.random.uniform(low=0, high=1, size=shape)}
     if lib == 'gunpowder':
         pass
     if lib == 'torchvision':
@@ -131,9 +170,10 @@ def transform_data(lib, data, pipeline):
         augm_data = pipeline(**data)
         return augm_data['image'].shape  # do something to enforce performing the action
     if lib == 'torchio':
-        pass
+        return pipeline(data).shape  # do something to enforce performing the action
     if lib == 'volum':
-        pass
+        augm_data = pipeline(**data)
+        return augm_data['image'].shape  # do something to enforce performing the action
     if lib == 'gunpowder':
         pass
     if lib == 'torchvision':
