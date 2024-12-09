ffd7e3fcc73dc890d9044d2eff987a1bac0cd455
Lucia D. Hradecka <lucia.d.hradecka@gmail.com>
2024-12-09 15:45:25 +0100

    speed tests (part 4): adding gunpowder

--- tests/speed_test_other.py
+++ tests/speed_test_other.py
@@ -24,18 +24,21 @@
 # ============================================================================================= #
 
 import time
+
+import funlib.persistence
 import numpy as np
 
-# bio-volumentations
-import bio_volumentations.core.composition as biovol_compose
-import bio_volumentations.augmentations as biovol
-
-# TorchIO: requires pytorch, torchIO (pip) (had networkx 2.8.8)
-import torchio
-# volumentations: require ???
-import volumentations
-# gunpowder: requires gunpowder (pip) (installed networkx 3.2.1)
-# import gunpowder
+# # bio-volumentations
+# import bio_volumentations.core.composition as biovol_compose
+# import bio_volumentations.augmentations as biovol
+#
+# # TorchIO: requires pytorch, torchIO (pip) (had networkx 2.8.8)
+# import torchio
+# # volumentations: with opencv (pip)
+# import volumentations
+# gunpowder: requires gunpowder (pip) (installed networkx 3.2.1), python>=3.10
+import gunpowder
+raw = gunpowder.ArrayKey('RAW')  # declare arrays to use in the pipeline
 
 # torchvision: requires pytorch, torchvision
 # import torchvision.transforms.v2 as torchvision_v2
@@ -44,15 +47,16 @@ import volumentations
 
 
 # libs = ['biovol', 'torchio', 'volum', 'gunpowder', 'torchvision', 'album']
-libs = ['biovol', 'torchio', 'volum']
-# libs = ['volum']
+# libs = ['volum', 'biovol', 'torchio']
+libs = ['gunpowder']
 
 # num_repeat = 100
 num_repeat = 10
 
 
 # image_shape_list = [(1, 256, 256, 256), (3, 256, 256, 256), (1, 256, 256, 256, 10), (3, 256, 256, 256, 10)]
-image_shape_list = [(1, 128, 128, 128)]
+image_shape_list = [(1, 256, 256, 256)]
+# image_shape_list = [(1, 128, 128, 128)]
 
 crop_shape = (100, 100, 100)
 pad_size = 6
@@ -66,61 +70,71 @@ noise_sigma_limits = (0.001, 1)
 brightness_limit = 0.2
 constrast_limit = 0.2
 
-# https://biovolumentations.readthedocs.io/latest/bio_volumentations.augmentations.html#
-augmentations_biovol = [
-    biovol.RandomCrop(shape=crop_shape, border_mode='constant', p=1),  # crop or pad
-    biovol.Pad(pad_size=pad_size, border_mode='constant', p=1),  # only pad
-    biovol.RandomFlip(axes_to_choose=None, p=1),
-    biovol.RandomAffineTransform(angle_limit=rotation_limit, translation_limit=translation_limit,
-                                 scaling_limit=scaling_limit, p=1),  # isotropic data
-    biovol.RandomAffineTransform(angle_limit=rotation_limit, translation_limit=translation_limit,
-                                 scaling_limit=scaling_limit, spacing=anisotropic_spacing, p=1),  # anisotropic data
-    biovol.RandomGaussianBlur(max_sigma=blur_sigma_limit, p=1),
-    biovol.GaussianNoise(var_limit=noise_sigma_limits, mean=0, p=1),
-    biovol.RandomBrightnessContrast(brightness_limit=brightness_limit, contrast_limit=constrast_limit, p=1),
-    biovol.Normalize(mean=0, std=1, p=1),
-]
-
-# https://torchio.readthedocs.io/
-augmentations_torchio = [
-    torchio.CropOrPad(target_shape=crop_shape, padding_mode=0, p=1),  # TODO center or random? no one knows
-    torchio.Pad(padding=pad_size, padding_mode=0, p=1),
-    torchio.RandomFlip(axes=(0, 1, 2), flip_probability=0.5, p=1),
-    torchio.RandomAffine(scales=scaling_limit, degrees=rotation_limit, translation=translation_limit, p=1),
-    # MISSING: anisotropic affine transform (their 'isotropic' has a different meaning)
-    torchio.RandomBlur(std=blur_sigma_limit, p=1),
-    torchio.RandomNoise(mean=0, std=noise_sigma_limits, p=1),
-    # MISSING: brightness contrast
-    torchio.ZNormalization(p=1)
-]
-
-# https://github.com/ZFTurbo/volumentations/blob/master/volumentations/augmentations/transforms.py
-# TODO: no generated docs + some classes are not documented at all (even in the code)
-rotate_limit_vol = (-22.5, 22.5)
-augmentations_volumentations = [
-    volumentations.RandomCrop(shape=crop_shape, p=1),  # TODO only crop
-    volumentations.PadIfNeeded(shape=pad_shape, border_mode='constant', p=1),  # TODO only pad ??
-    volumentations.Flip(axis=None, p=1),  # this is random variant (no deterministic exists)
-    # MISSING: affine and translation -> using a sequence of rotation and scale at least
-    [volumentations.RandomScale(scaling_limit[:2], p=1),
-     volumentations.Rotate(x_limit=rotate_limit_vol, y_limit=rotate_limit_vol, z_limit=rotate_limit_vol, p=1)],
-    # MISSING: anisotropic affine (or anisotropic anything)
-    # MISSING: implementation of Blur ... volumentations.Blur(blur_limit=(3, 7), p=1),  # TODO: unclear meaning of param blur_limit; is it Gaussian? / alternative: GlassBlur - but docs say it's noise + it isn't gaussian blur only
-    volumentations.GaussianNoise(var_limit=noise_sigma_limits, mean=0, p=1),
-    volumentations.RandomBrightnessContrast(brightness_limit=brightness_limit, contrast_limit=constrast_limit, p=1),
-    volumentations.Normalize(range_norm=False, p=1),
-]
-
 
 def get_transformation_list(lib):
     if lib == 'biovol':
-        return augmentations_biovol
+        # https://biovolumentations.readthedocs.io/latest/bio_volumentations.augmentations.html#
+        return [
+            biovol.RandomCrop(shape=crop_shape, border_mode='constant', p=1),  # crop or pad
+            biovol.Pad(pad_size=pad_size, border_mode='constant', p=1),  # only pad
+            biovol.RandomFlip(axes_to_choose=None, p=1),
+            biovol.RandomAffineTransform(angle_limit=rotation_limit, translation_limit=translation_limit,
+                                         scaling_limit=scaling_limit, p=1),  # isotropic data
+            biovol.RandomAffineTransform(angle_limit=rotation_limit, translation_limit=translation_limit,
+                                         scaling_limit=scaling_limit, spacing=anisotropic_spacing, p=1),
+            # anisotropic data
+            biovol.RandomGaussianBlur(max_sigma=blur_sigma_limit, p=1),
+            biovol.GaussianNoise(var_limit=noise_sigma_limits, mean=0, p=1),
+            biovol.RandomBrightnessContrast(brightness_limit=brightness_limit, contrast_limit=constrast_limit, p=1),
+            biovol.Normalize(mean=0, std=1, p=1),
+        ]
     if lib == 'torchio':
-        return augmentations_torchio
+        # https://torchio.readthedocs.io/
+        return [
+            torchio.CropOrPad(target_shape=crop_shape, padding_mode=0, p=1),  # TODO center or random? no one knows
+            torchio.Pad(padding=pad_size, padding_mode=0, p=1),
+            torchio.RandomFlip(axes=(0, 1, 2), flip_probability=0.5, p=1),
+            torchio.RandomAffine(scales=scaling_limit, degrees=rotation_limit, translation=translation_limit, p=1),
+            # MISSING: anisotropic affine transform (their 'isotropic' has a different meaning)
+            torchio.RandomBlur(std=blur_sigma_limit, p=1),
+            torchio.RandomNoise(mean=0, std=noise_sigma_limits, p=1),
+            # MISSING: brightness contrast
+            torchio.ZNormalization(p=1)
+        ]
     if lib == 'volum':
-        return augmentations_volumentations
+        # https://github.com/ZFTurbo/volumentations/blob/master/volumentations/augmentations/transforms.py
+        # TODO: no generated docs + some classes are not documented at all (even in the code)
+        rotate_limit_vol = (-22.5, 22.5)
+        return [
+            volumentations.RandomCrop(shape=crop_shape, p=1),  # TODO only crop
+            volumentations.PadIfNeeded(shape=pad_shape, border_mode='constant', p=1),  # TODO only pad ??
+            volumentations.Flip(axis=None, p=1),  # this is random variant (no deterministic exists)
+            # MISSING: affine and translation -> using a sequence of rotation and scale at least
+            [volumentations.RandomScale(scaling_limit[:2], p=1),
+             volumentations.Rotate(x_limit=rotate_limit_vol, y_limit=rotate_limit_vol, z_limit=rotate_limit_vol, p=1)],
+            # MISSING: anisotropic affine (or anisotropic anything)
+            # MISSING: implementation of Blur ... volumentations.Blur(blur_limit=(3, 7), p=1),  # TODO: unclear meaning of param blur_limit; is it Gaussian? / alternative: GlassBlur - but docs say it's noise + it isn't gaussian blur only
+            volumentations.GaussianNoise(var_limit=noise_sigma_limits, mean=0, p=1),
+            volumentations.RandomBrightnessContrast(brightness_limit=brightness_limit, contrast_limit=constrast_limit,
+                                                    p=1),
+            volumentations.Normalize(range_norm=False, p=1),
+        ]
     if lib == 'gunpowder':
-        pass
+        # https://funkelab.github.io/gunpowder/
+        return [
+            [gunpowder.Crop(key=raw, roi=gunpowder.Roi((0,) * 4, (1,) + crop_shape)), gunpowder.RandomLocation()], # only random crop  # TODO this probably operates at smaller image from the begining
+            gunpowder.Pad(key=raw, size=gunpowder.Coordinate((0,) + (pad_size,) * 3)),  # only pad  # TODO this pads the image but only retrieves the original-sized image at the end
+            gunpowder.SimpleAugment(transpose_probs=[0, 0, 0, 0], p=1),  # only mirror=flip
+            # MISSING: affine (only have DeformAugment = elastic transform (rotation, scaling, jitter; needs physical units)
+            # MISSING: affine anisotropic
+            # MISSING: blur
+            gunpowder.NoiseAugment(array=raw, mode='gaussian', clip=False, p=1),
+            # TODO image should be of type float and within range [-1, 1] or [0, 1]
+            gunpowder.IntensityAugment(array=raw, scale_min=1 - constrast_limit, scale_max=1 + constrast_limit,
+                                       shift_min=-brightness_limit, shift_max=brightness_limit, z_section_wise=False,
+                                       clip=False, p=1),
+            # MISSING: normalize to 0 mean and 1 variance ... there's only normalization to [0,1] interval: gunpowder.Normalize(array='raw')
+        ]
     if lib == 'torchvision':
         pass
     if lib == 'album':
@@ -139,7 +153,10 @@ def init_compose(lib, transform):
         else:
             return volumentations.Compose([transform], p=1.0)
     if lib == 'gunpowder':
-        pass
+        if isinstance(transform, list):
+            return transform[0] + transform[1]  # hardcoded but the easiest way at the moment
+        else:
+            return transform
     if lib == 'torchvision':
         pass
     if lib == 'album':
@@ -154,13 +171,26 @@ def get_input_data(lib, shape):
         return {'image': np.random.uniform(low=0, high=1, size=shape)}
     if lib == 'torchio':
         # (C, W, H, D)
-        shape_torchio = (shape[0], shape[3], shape[2], shape[1])  # np.asarray(shape).transpose((0, 3, 2, 1))
+        shape_torchio = (shape[0], shape[3], shape[2], shape[1])
         return np.random.uniform(low=0, high=1, size=shape_torchio)
     if lib == 'volum':
-        # TODO shape
-        return {'image': np.random.uniform(low=0, high=1, size=shape)}
+        # (H, W, D, [C]) or (D, H, W, [C])
+        shape_volum = (shape[2], shape[3], shape[1], shape[0])
+        return {'image': np.random.uniform(low=0, high=1, size=shape_volum)}
     if lib == 'gunpowder':
-        pass
+        # ([C], [D], H, W)
+
+        # source = gunpowder.ArraySource(key=raw, array=gunpowder.Array(np.random.uniform(low=0, high=1, size=shape),
+        #                                                               spec=gunpowder.ArraySpec()))
+        source = gunpowder.ArraySource(key=raw,
+                                       array=funlib.persistence.Array(np.random.uniform(low=0, high=1, size=shape)))
+
+        # formulate a request for "raw"
+        request = gunpowder.BatchRequest()
+        request[raw] = gunpowder.Roi((0,)*len(shape), shape)  # an offset and a size
+        # request[raw] = gunpowder.Roi((0,)*4, (None,)*4)  # TODO can I get "the whole image"?
+
+        return {'source': source, 'request': request}
     if lib == 'torchvision':
         pass
     if lib == 'album':
@@ -177,7 +207,12 @@ def transform_data(lib, data, pipeline):
         augm_data = pipeline(**data)
         return augm_data['image'].shape  # do something to enforce performing the action
     if lib == 'gunpowder':
-        pass
+        whole_pipeline = data['source'] + pipeline
+        if 'Crop' in str(pipeline):
+            data['request'][raw] = gunpowder.Roi((0,)*(1+len(crop_shape)), (1,) + crop_shape)
+        with gunpowder.build(whole_pipeline):
+            batch = whole_pipeline.request_batch(data['request'])
+        return batch[raw].data  # TODO what is this data?
     if lib == 'torchvision':
         pass
     if lib == 'album':
